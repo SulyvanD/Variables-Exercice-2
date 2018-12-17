85ddf24a4088b4048be8a3171da1e4d2e4523c8b
SulyvanD <sulyvan.debraize@net-c.com>
2018-12-17 14:46:02 +0100

    Exercice completé

---
+++ Variables - Exercice 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables___Exercice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z, resultat;
+
+            x = 3;
+            y = 4;
+            z = 5;
+
+            Console.WriteLine("Les valeurs de X, Y et Z sont " + x + ", " + y + " et " + z);
+            resultat = (x + y) * z;
+            Console.WriteLine("Le résultat de '(X + Y) x Z' est " + resultat);
+        }
+    }
+}
